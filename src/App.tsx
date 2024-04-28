1e0d8f7284169a33c2e8032da1a5d11719158e8d
FanomezanaNat <hei.fanomezana.2@gmail.com>
2024-04-28 23:05:17 +0300

    feat: add crud for posts,users,comments

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import {
+  Admin,
+  EditGuesser,
+  ListGuesser,
+  Resource,
+  ShowGuesser
+} from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { PostList, PostShow, PostEdit } from "./Post"
+import { CommentEdit, CommentList, CommentShow } from "./Comment";
+
+export const App = () =>
+  <Admin dataProvider={dataProvider} >
+    <Resource name='users' list={ListGuesser} edit={EditGuesser} show={ShowGuesser}  />
+    <Resource name="posts" list={PostList} show={PostShow} edit={PostEdit} />
+    <Resource name="posts" list={PostList} show={PostShow} edit={PostEdit} />
+
+    <Resource name="comments" list={CommentList} show={CommentShow} edit={CommentEdit} />
+  </Admin>;
